e9ef7c4513cbd9ff7d60a74de9d7754c92a7dbf5
AndoniainaNantenaina <ando.andoniaina.nantenaina@esti.mg>
2025-11-28 12:38:22 +0300

    ruff added

--- main.py
+++ main.py
@@ -3,9 +3,15 @@ import streamlit as st
 pg = st.navigation(
     {
         "Timesheet": [
-            st.Page(title="Home", page="pages/Home.py", icon=":material/home:"),
             st.Page(
-                title="Projects", page="pages/Projects.py", icon=":material/folder:"
+                title="Home",
+                page="pages/Home.py",
+                icon=":material/home:",
+            ),
+            st.Page(
+                title="Projects",
+                page="pages/Projects.py",
+                icon=":material/folder:",
             ),
             st.Page(
                 title="Calendar",
